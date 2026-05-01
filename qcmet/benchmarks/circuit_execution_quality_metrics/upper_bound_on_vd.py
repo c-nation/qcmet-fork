Fix calculation of num trap circuits

qcmet/benchmarks/circuit_execution_quality_metrics/upper_bound_on_vd.py
@@ -68,7 +68,7 @@ class UpperBoundOnVD(BaseBenchmark):
         self.config["eta"] = eta
         self.config["qubits"] = self.qubits
         self.config["num_trap_circuits"] = int(
-            np.ceil(2 * np.log((2 / (1 - eta)) / mu**2))
+            np.ceil(2 * np.log((2 / (1 - eta))) / mu**2)
         )
 
         if seed is None: